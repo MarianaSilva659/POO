130270ac192b90cd5b748b6b57356fd632443955
jpcg27 <a100747@alunos.uminho.pt>
2023-04-21 15:58:24 +0100

    Tshirt é abstract

--- src/TShirt.java
+++ src/TShirt.java
@@ -2,16 +2,9 @@ import java.util.Objects;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TShirt extends Artigo{
+public abstract class TShirt extends Artigo{
     private String tamanho;
     private String padrao;
-    private static final Map<String, Integer> mapDesconto = new HashMap<>();
-
-    static{
-        mapDesconto.put("liso", 0);
-        mapDesconto.put("riscas", 50);
-        mapDesconto.put("palmeiras", 50);
-    }
 
     public TShirt(String tamanho, String padrao) {
         this.tamanho = tamanho;
@@ -65,18 +58,6 @@ public class TShirt extends Artigo{
         return Objects.hash(super.hashCode(), getTamanho(), getPadrao());
     }
 
-    public TShirt clone(){
-        return new TShirt(this);
-    }
-    public double precoartigo(){
-        double preco = 0;
-        if(getNumeroDonos() > 0){
-            preco = getPrecoBase() * ((100 - mapDesconto.get(this.getPadrao()))/100);
-        }  
-        else{
-            preco = getPrecoBase() * ((100 - getDesconto())/100);
-        } 
-        return preco;
-    }
+    public abstract double precoartigo();
 }
 
