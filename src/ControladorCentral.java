c6129eb84ecaf1cd6d5f714c8711032c45814cfb
Mariana <a100702@alunos.uminho.pt>
2023-05-05 10:32:51 +0100

    novos desenvolvimentos

--- src/ControladorCentral.java
+++ src/ControladorCentral.java
@@ -11,16 +11,19 @@ public class ControladorCentral {
 
                 case 1:
                 //pedir o email e password já com conta
-                break;
+                    break;
 
                 case 2:
                 //criar conta pedir email e password
-                break;
+                    break;
 
                 case 0:
-                System.out.println("Execução interrompida");
-                System.exit(0);
-
+                    System.out.println("Execução interrompida");
+                    System.exit(0);
+                    break;
+                default: 
+                    System.out.println("Inseriu um número incorreto. Vintege exit");
+                    System.exit(0);
             }
         }
     }
